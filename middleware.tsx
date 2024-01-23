7e73b1fc2560637ab216ee3690514a4430831804
a1013457richar <r11722013@g.ntu.edu.tw>
2024-01-23 19:56:19 +0800

    middlewrae

--- middleware.tsx
+++ middleware.tsx
@@ -8,7 +8,7 @@ export default authMiddleware({
   publicRoutes: ["/api/webhook/clerk","/api/uploadthing"],
 
   // An array of routes to be ignored by the authentication middleware.
-  ignoredRoutes: [],
+  ignoredRoutes: ['/api/{routeName}'],
 });
 
 export const config = {
